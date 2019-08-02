11588ea159e92efa286586e33089d15246b49d42
Guillaume BOUDON <guillaumeboudon@gmail.com>
2019-08-02 14:55:05 +0200

    :wrench: Implement webpack mode

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -5,33 +5,58 @@ const rootPath = path.resolve(__dirname, '..')
 const srcPath  = path.join(rootPath, 'src')
 const distPath = path.join(rootPath, 'dist')
 
-module.exports = {
-  entry: path.join(srcPath, 'index.js'),
-  output: {
-    filename: 'bundle.js',
-    path: distPath
-  },
-  module: {
-    noParse: /\.elm$/,
-    rules: [
-      {
-        test: /\.elm$/,
-        exclude: [/elm-stuff/, /node_modules/],
-        use: [
-          { loader: 'elm-webpack-loader',
-            options: {
-              debug: true,
-              forceWatch: true,
-              verbose: true
-            }
-          }
-        ]
+const isProd = (mode) => mode === 'production'
+const isDev  = (mode) => !isProd(mode)
+
+
+/* ============================================================================
+ * > SHARED
+ * ============================================================================
+ */
+const entry = path.join(srcPath, 'index.js')
+
+const output = (mode) => ({
+  filename: isProd(mode) ? '[name]-[hash].js' : '[name].js',
+  path: distPath
+})
+
+const moduleRuleElm = (mode) => ({
+  test: /\.elm$/,
+  exclude: [/elm-stuff/, /node_modules/],
+  use: [
+    { loader: 'elm-webpack-loader',
+      options: {
+        debug: isDev(mode),
+        forceWatch: isDev(mode),
+        verbose: isDev(mode)
       }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(srcPath, 'html/main.html')
-    })
+    }
   ]
+})
+
+const pluginHtmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: path.join(srcPath, 'html/main.html')
+})
+
+
+/* ============================================================================
+ * > MAIN
+ * ============================================================================
+ */
+module.exports = (env, argv) => {
+  const mode = argv.mode
+
+  return {
+    entry: entry,
+    output: output(mode),
+    module: {
+      noParse: /\.elm$/,
+      rules: [
+        moduleRuleElm(mode)
+      ]
+    },
+    plugins: [
+      pluginHtmlWebpackPlugin
+    ]
+  }
 }
